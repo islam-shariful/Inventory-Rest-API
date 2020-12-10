fdd3919455fb0192de69776084a9168a020db43e
Md. Shariful Islam <imdshariful171@gmail.com>
2020-12-11 00:17:18 +0600

    'Get(id)->Products' Added

---
+++ Inventory Rest API/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Inventory_Rest_API.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Inventory_Rest_API.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        ProductRepository productRepository = new ProductRepository();
+
+        public IHttpActionResult GetAll()
+        {
+            return Ok(productRepository.GetAll());
+        }
+    }
+}
