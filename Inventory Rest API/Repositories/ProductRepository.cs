72ca45a41a67aa4af11626ab0fee493b19b59fe8
Md. Shariful Islam <imdshariful171@gmail.com>
2020-12-10 15:07:49 +0600

    Repositories Added

---
+++ Inventory Rest API/Repositories/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Inventory_Rest_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory_Rest_API.Repositories
+{
+    public class ProductRepository : Repository<Product>
+    {
+        public List<Product> GetTopProducts(int top)
+        {
+            //return this.context.Products.OrderBy(x => x.Price).Take(2).ToList();
+            return this.GetAll().OrderByDescending(x => x.Price).Take(top).ToList();
+        }
+    }
+}
