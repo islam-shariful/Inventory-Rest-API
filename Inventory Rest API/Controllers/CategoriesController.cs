44571e64ee8ea3dcc7114f5364c7a9a309e3f57a
Md. Shariful Islam <imdshariful171@gmail.com>
2020-12-16 12:44:55 +0600

    Route prefix->ProductController

--- Inventory Rest API/Controllers/CategoriesController.cs
+++ Inventory Rest API/Controllers/CategoriesController.cs
@@ -9,13 +9,16 @@ using System.Web.Http;
 
 namespace Inventory_Rest_API.Controllers
 {
+    [RoutePrefix("api/categories")]
     public class CategoriesController : ApiController
     {
         CategoryRepository categoryRepository = new CategoryRepository();
+        [Route("")]
         public IHttpActionResult GetAll()
         {
             return Ok(categoryRepository.GetAll());
         }
+        [Route("{id}", Name="GetCategoryById")]
         public IHttpActionResult GetAll(int id)
         {
             var category = categoryRepository.Get(id);
@@ -28,23 +31,27 @@ namespace Inventory_Rest_API.Controllers
                 return Ok(categoryRepository.Get(id));
             }
         }
+        [Route("")]
         public IHttpActionResult Post(Category category)
         {
             categoryRepository.Insert(category);
-            return Created("api/Categories/" + category.CategoryId, category);
+            string uri = Url.Link("GetCategoryById", new { id = category.CategoryId });
+            return Created(uri, category);
         }
+        [Route("{id}")]
         public IHttpActionResult Put([FromUri] int id,[FromBody]Category category)
         {
             category.CategoryId = id;
             categoryRepository.Update(category);
             return Ok(category);
         }
+        [Route("{id}")]
         public IHttpActionResult Delete(int id)
         {
             categoryRepository.Delete(id);
             return StatusCode(HttpStatusCode.NoContent);
         }
-        [Route("api/categories/{id}/products")]
+        [Route("{id}/products")]
         public IHttpActionResult GetProductsByCategoryId(int id)
         {
             ProductRepository productRepository = new ProductRepository();            
